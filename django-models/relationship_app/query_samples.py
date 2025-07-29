335ad5050fcfcbda80b037477ed73cdb940208c8
mukeshimanafrank <franck1franklin@gmail.com>
2025-07-29 07:46:21 +0000

    Add relationship_app with advanced Django model relationships

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,11 +1,9 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    """Query all books by a specific author."""
     return Book.objects.filter(author__name=author_name)
 
 def get_books_in_library(library_name):
-    """List all books in a library."""
     try:
         library = Library.objects.get(name=library_name)
         return library.books.all()
@@ -13,7 +11,6 @@ def get_books_in_library(library_name):
         return []
 
 def get_librarian_for_library(library_name):
-    """Retrieve the librarian for a library."""
     try:
         library = Library.objects.get(name=library_name)
         return library.librarian
