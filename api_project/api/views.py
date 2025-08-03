e9d23ccc614cc2ab4777a32b73f659d091c9e3b4
mukeshimanafrank <franck1franklin@gmail.com>
2025-08-03 10:19:26 +0000

    Fix project structure: api_project with settings.py in correct place

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,6 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):  # Change here to ListAPIView
     queryset = Book.objects.all()
     serializer_class = BookSerializer
