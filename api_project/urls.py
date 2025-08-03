89c40f229c665cba24a6e40d297928fba7e9b9e9
mukeshimanafrank <franck1franklin@gmail.com>
2025-08-03 10:24:52 +0000

    Full update of api_project/urls.py to include api app URLs

--- api_project/urls.py
+++ api_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Make sure to import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),  # Route all /api/ URLs to your api app
 ]
