60e45771cccf4222eef3a7b2aabb39669f8eba3d
mukeshimanafrank <franck1franklin@gmail.com>
2025-08-19 09:26:07 +0000

    Added register, login, and profile routes in accounts app

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="register"),
-    path("login", LoginView.as_view(), name="login"),
-    path("profile", ProfileView.as_view(), name="profile"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),  # ✅ include accounts routes
 ]
