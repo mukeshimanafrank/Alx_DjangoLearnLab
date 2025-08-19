a0ef7e202a705738175386af8e6a4ef9959389ce
mukeshimanafrank <franck1franklin@gmail.com>
2025-08-19 09:50:08 +0000

    Update accounts views: use GenericAPIView, add ListUsersView and follow/unfollow features

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,13 +2,19 @@ from rest_framework import viewsets, permissions, filters, generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
+# ---------------------------
+# Permissions
+# ---------------------------
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """Allow owners to edit/delete, read-only for others"""
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
 
-
+# ---------------------------
+# Posts CRUD
+# ---------------------------
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
@@ -19,7 +25,9 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
+# ---------------------------
+# Comments CRUD
+# ---------------------------
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by("-created_at")
     serializer_class = CommentSerializer
@@ -28,12 +36,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
-# Feed endpoint
+# ---------------------------
+# Feed
+# ---------------------------
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        return Post.objects.filter(author__in=user.following.all()).order_by("-created_at")
+        following_users = user.following.all()  # users that the current user follows
+        return Post.objects.filter(author__in=following_users).order_by("-created_at")
