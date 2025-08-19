7ed89a6053550a7baf35e6e23d74de50e6b68156
mukeshimanafrank <franck1franklin@gmail.com>
2025-08-19 09:51:10 +0000

    Update accounts views: use GenericAPIView, add ListUsersView and follow/unfollow features

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,4 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
 
+# Create router and register viewsets
 router = DefaultRouter()
+router.register(r"posts", PostViewSet, basename="post")
+router.register(r"comments", CommentViewSet, basename="comment")
+
+urlpatterns = [
+    # Include router URLs
+    path("", include(router.urls)),
+
+    # Feed endpoint
+    path("feed/", FeedView.as_view(), name="feed"),
+]
