0da5fdcab17a3367ebdb276d5495f71fe829321e
mukeshimanafrank <franck1franklin@gmail.com>
2025-08-19 09:16:14 +0000

    Setup accounts app, custom user model, DRF auth, and updated settings.py

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,27 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+from .serializers import RegisterSerializer, LoginSerializer, UserProfileSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
