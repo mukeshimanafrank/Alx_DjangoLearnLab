e07e649a9eb4f4c0310fcf24a1374ac2a4d1171a
mukeshimanafrank <franck1franklin@gmail.com>
2025-08-19 09:31:48 +0000

    Add posts and comments models, serializers, views, urls with CRUD and pagination

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include
+
+
 
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("api/", include("posts.urls")),  # ✅ add posts endpoints
 ]
